8d0658af96d8b8eb96237a998c842025aed52d87
Arkadiy <arkadiy@ex.ua>
2019-02-04 21:24:46 +0200

    base project

---
+++ README.md
@@ -0,0 +1,10 @@
+# react-webpack-base-project
+
+## Base web project using React framework
+
+### #react, #webpack, #babel, #express
+
+To start project, do `npm i` and then `npm run dev`
+
+---------
+Ready to deploy on Heroku
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import '@babel/polyfill';
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = (
+  <main>
+    <h1>Hello world!</h1>
+    <p>Edit <code>src/index.js</code> to start</p>
+  </main>
+);
+
+ReactDOM.render(root, document.getElementById('root'));
